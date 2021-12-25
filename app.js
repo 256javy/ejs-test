7d73c49090693e3490d31f31f1b404d522cc05fb
256javy <256javiervillalba@gmail.com>
2021-12-25 12:12:47 -0300

    looping in ejs. using partials in ejs

--- app.js
+++ app.js
@@ -5,18 +5,31 @@ const port = 3000
 app.set('view engine', 'ejs')
 
 
+
+const posts = [
+    {title: 'Title 1', body: 'Body 1' },
+    {title: 'Title 2', body: 'Body 2' },
+    {title: 'Title 3', body: 'Body 3' },
+    {title: 'Title 4', body: 'Body 4' },
+]
+
 const user = {
     firstName: 'Tim',
     lastName: 'Cook',
     amdin : true,
 }
+
 app.get('/', (req, res) => {
     res.render('pages/index', {
         user: user
     })
 })
 
-
+app.get('/articles', (req, res) => {
+    res.render('pages/articles', {
+        articles: posts
+    })
+})
 
 app.listen(port, () => {
   console.log(`App listening at port ${port}`)
